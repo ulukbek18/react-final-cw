23df3f69fd4a92a0c70e085b748bbe71e4c902c3
Ulukbek <orozgozhoev.u@gmail.com>
2022-09-16 18:35:27 +0600

    tried to create a form that will create a massive by template set, before sending it to payload, need to rework

--- react-cw/src/components/CreateQuestion.jsx
+++ react-cw/src/components/CreateQuestion.jsx
@@ -1,79 +1,134 @@
 import React, { useState } from 'react';
 import 'antd/dist/antd.css';
-import {
-  Button,
-  Form,
-  Input,
-  Switch,
-} from 'antd';
+import { Button, Form, Input, Switch, } from 'antd';
 
 
 const CreateQuestion = () => {
-  const [componentSize, setComponentSize] = useState('default');
-  const [isCheckedTrue, setIsCheckedTrue]=useState(false)
+    const [componentSize, setComponentSize] = useState('default');
+    const [isCheckedTrue, setIsCheckedTrue] = useState(false)
+    const [answers, setAnswers] = useState(initStateAnswers)
+    const initStateAnswers = [
+        {
+            id: 1,
+            question: "",
+            multiple: false,
+            answers: [
+                {
+                    answer: "",
+                    correct: false
+                },
 
-  const onFormLayoutChange = ({ size }) => {
-    setComponentSize(size);
-  };
+                {
+                    answer: "",
+                    correct: false
+                },
+                {
+                    answer: "",
+                    correct: false
+                },
+                {
+                    answer: "",
+                    correct: false
+                },
+            ]
+        }
+    ]
 
- const onChange=(checked)=>{
-    setIsCheckedTrue(checked)
-    console.log('checked',checked)
- }
-
-
-
-
-
-  const type = isCheckedTrue ? 'checkbox' : 'radio'
-
-  return (
-    <Form
-      labelCol={{
-        span: 4,
-      }}
-      wrapperCol={{
-        span: 14,
-      }}
-      layout="horizontal"
-      initialValues={{
-        size: componentSize,
-      }}
-      onValuesChange={onFormLayoutChange}
-      size={componentSize}
-    >
-
-
-      <Form.Item label="Имеет несколько значений" valuePropName="checked" >
-        <Switch  onChange={onChange}/>
-      </Form.Item>
-      
-
-
-
-      <Form.Item label="Вопрос">
-        <Input />
+    const handleAnswersInput = (e, index) => {
+        const { value } = e.target
+        const newAnswers = [...answers]
+        newAnswers.question = value
         
-      </Form.Item>
-      <Form.Item label="Вариант ответа (1)">
-        <Input type={type}/>
-      </Form.Item>
-      <Form.Item label="Вариант ответа (2)">
-        <Input type={type}/>
-      </Form.Item>
-      <Form.Item label="Вариант ответа (3)">
-        <Input type={type}/>
-      </Form.Item>
-      <Form.Item label="Вариант ответа (4)">
-        <Input type={type}/>
-      </Form.Item>
-      
-      
-      <Form.Item >
-        <Button>Создать вопрос</Button>
-      </Form.Item>
-    </Form>
-  );
+        setAnswers(newAnswers)
+      }
+
+      const handleAnswersCheckbox = (e, index) => {
+        const {checked} = e.target
+        const newAnswers = [...answers]
+        newAnswers.answers[0].correct = checked
+        
+        setAnswers(newAnswers)
+      }
+      console.log('answers',answers)
+
+
+    const onChange = (checked) => {
+        setIsCheckedTrue(checked)
+        console.log('checked', checked)
+    }
+
+    const type = isCheckedTrue ? 'checkbox' : 'radio'
+
+    return (
+        <div>
+
+            <Form
+                labelCol={{
+                    span: 4,
+                }}
+                wrapperCol={{
+                    span: 14,
+                }}
+                layout="horizontal"
+                initialValues={{
+                    size: componentSize,
+                }}
+                // onValuesChange={onFormLayoutChange}
+                size={componentSize}
+            >
+
+
+                <Form.Item label="Имеет несколько значений" valuePropName="checked" >
+                    <Switch onChange={onChange} />
+                </Form.Item>
+
+
+
+
+                <Form.Item label="Вопрос">
+                    <Input />
+
+                </Form.Item>
+                <div className='question-input'>
+                    <Form.Item label="Вариант ответа (1)">
+                        <Input type='text' 
+                        onChange={(e) => handleAnswersInput(e)}/>
+                        <Input type={type}
+                            name='answer'
+                            id={1}  
+                            onChange={(e) => handleAnswersCheckbox(e)}/>
+                    </Form.Item>
+
+                </div>
+                <Form.Item label="Вариант ответа (2)">
+                    <Input type='text' />
+                    <Input type={type}
+                        name='answer'
+                        id={2} />
+                </Form.Item>
+                <Form.Item label="Вариант ответа (3)">
+                    <Input type='text' />
+                    <Input type={type}
+                        name='answer' />
+                </Form.Item>
+                <Form.Item label="Вариант ответа (4)">
+                    <Input type='text' />
+                    <Input type={type}
+                        name='answer'
+                        id={3} />
+                </Form.Item>
+
+                <Form.Item >
+                    <Button>Создать вопрос</Button>
+                </Form.Item>
+            </Form>
+            <div>
+                <button> Начать Тест</button>
+            </div>
+        </div>
+
+
+    );
 };
 
 
